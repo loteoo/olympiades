d933a89cf218358bdf9e736a8e082084b02ade2c
Alexandre Lotte <alexlotte16@gmail.com>
2018-03-29 20:08:21 -0400

    fichiers supp

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import {view} from './views/'
 
 
 // Start hyperapp
-window.main = app(state, actions, view, document.querySelector("main"));
+window.main = app(state, actions, view, document.body);
 
 // Activate our router
 const unsubscribe = location.subscribe(main.location);
