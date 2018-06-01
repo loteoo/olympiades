e86207da0f884325dd19f6ae05b477618d956351
Alexandre Lotte <alexlotte16@gmail.com>
2018-05-31 21:48:08 -0400

    cleanup

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-import {Link, Route, Switch, Redirect, location} from "@hyperapp/router"
-
-export const actions = {
-  location: location.actions
-}
--- src/state/index.js
+++
@@ -1,5 +0,0 @@
-import {Link, Route, Switch, Redirect, location} from "@hyperapp/router"
-
-export const state = {
-  location: location.state
-}
